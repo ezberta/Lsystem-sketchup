ca148a407e16c4b17e5831fa7fe268243877259d
eb3x <eb3x>
2019-01-31 08:19:35 -0700

    add github address to description

--- Lsystem.rb
+++ Lsystem.rb
@@ -15,8 +15,10 @@ module CommunityExtensions
 
     unless file_loaded?(__FILE__)
       ex = SketchupExtension.new('L-System', 'Lsystem/main')
-      ex.description = 'L-System generator'
-      ex.version     = '0.5.1'
+      ex.description = 'L-System generator. ' <<
+                       'See https://github.com/ezberta/Lsystem-sketchup ' <<
+                       ' for usage instructions.'
+      ex.version     = '0.5.2'
       ex.copyright   = '2019 Eugene Berta, ' <<
                        'released under the MIT License'
       ex.creator     = 'Eugene Berta'
